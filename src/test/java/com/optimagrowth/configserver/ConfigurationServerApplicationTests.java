73992a0b3a9d40c0a9b0bf3604b1d9de87b3e2a9
Sean Maxwell <sa.maxwell@yahoo.com>
2022-02-08 21:42:32 -0600

    Dockerized config service

--- src/test/java/com/optimagrowth/configserver/ConfigurationServerApplicationTests.java
+++ src/test/java/com/optimagrowth/configserver/ConfigurationServerApplicationTests.java
@@ -2,7 +2,10 @@ package com.optimagrowth.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+/* Use native profile to look for configs on disk */
+@ActiveProfiles("native")
 @SpringBootTest
 class ConfigurationServerApplicationTests {
 
